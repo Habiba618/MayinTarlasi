fcc672c771d5d535ce73e270174af5ea5627f315
Habiba618 <82892084+Habiba618@users.noreply.github.com>
2023-12-22 12:29:13 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc=new Scanner(System.in);
+        int row, col;
+             System.out.print("Mayin Tarlasina hosgeldiniz......");
+        System.out.println("Oynamak istedigniz boyutlari giriniz: ");
+        System.out.print("Satir sayisi: ");
+        row=sc.nextInt();
+        System.out.print("Sutun sayisi: ");
+        col=sc.nextInt();
+        MayinTarlasi mayin = new MayinTarlasi(row,col);
+        mayin.run();
+
     }
 }
